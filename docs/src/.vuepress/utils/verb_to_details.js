f2ee0959d1f60e6232ab49fbef3b35569b31ba0d
Kartik <kartik.arora1214@gmail.com>
2021-09-30 12:39:58 +0530

    new sheet for dictionary verbs

--- docs/src/.vuepress/utils/verb_to_details.js
+++ docs/src/.vuepress/utils/verb_to_details.js
@@ -1,9 +1,25 @@
 function generateVerbContent(verb) {
-    return `## ${verb.verb}
-    Current Weight: ${verb.weight}
+    return `# ${verb.verb}
+${verb.description}
 
-    Rubie Value: ${verb.rubies}
-    `;
+## Value Exchange
+Current Weight: ${verb.weight}
+
+Rubie Value: ${verb.rubies}
+
+## Expected Value
+${verb.expected_value}
+\`\`\`json
+${verb.value_example}
+\`\`\`
+
+## Expected Actor
+${verb.user_by}
+\`\`\`
+${verb.user_by_example}
+\`\`\`
+
+`;
 }
 
 module.exports = {
