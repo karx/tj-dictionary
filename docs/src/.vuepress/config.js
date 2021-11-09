15bef451c14ffda254acf9cde0ddcce76e07f879
Rohit Negi <negi.rohit@edvanta.com>
2021-11-09 07:59:34 +0000

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -104,7 +104,7 @@ module.exports = async () => {
               'explore',
               'forcegraph',
               'notes',
-              'Search'
+              'Search',
               'wikidata',
               'wolframalpha'
             ]
