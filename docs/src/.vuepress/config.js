1152dddef12c7d4beaad424cf2095d1639cd4fe8
Rohit Negi <negi.rohit@edvanta.com>
2021-12-24 10:48:15 +0000

    Added 'registration' to the menu of 'Stories' page

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -121,6 +121,7 @@ module.exports = async () => {
             children: [
               '',
               'invitation',
+              'registration',
               'thought-map',
             ]
           }
